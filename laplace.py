71486ad2c8f70fa537c205b1122c7ef256df8aaf
Vinay <vprita@cs.washington.edu>
2024-01-23 00:26:35 -0800

    Used some of anthony's ideas and redid my implementation

--- laplace.py
+++ laplace.py
@@ -1,65 +1,56 @@
 import numpy as np
 import math
 from PIL import Image
+import cv2
 
-# Get image and convert to 2D array
 im = Image.open("world3.png")
+im = im.resize((130,70))
 image = np.asarray(im)
 
-start = [5, 5]
-end = [670, 370]
+start = [3, 3]
+end = [125, 68]
+#start = [5, 5]
+#end = [670, 370]
 
-def averagePoints(boundary_c, voltage):
-    new_voltage = np.copy(voltage)
-    for i in range(1, len(boundary_c) - 1):
-        for j in range(1, len(boundary_c[0]) - 1):
-            if not boundary_c[i][j] == 1:
-                new_voltage[i][j] = 0.25 * (voltage[i][j+1] +voltage[i+1][j] +  voltage[i-1][j] + voltage[i][j-1])
-    return new_voltage
-
-# Laplace Algorithm
 def Laplace(image, start, end):
     height = len(image)
     length = len(image[0])
+    potential_map = np.zeros((height, length))
+    potential_map.fill(255)
+    #potential_map[start[1]][start[0]] = 0 
+    potential_map[end[1]][end[0]] = 0
 
-    boundary_c = np.zeros((height, length), dtype=int)
-    voltage = np.zeros((height, length), dtype=float)
+    boundary = []
+    for y in range(height):
+        for x in range(length):
+            if(image[y][x][0] == 0):
+                boundary.append([y,x])
 
-    for i in range(len(image)):
-        for j in range(len(image[0])):
-            if image[i][j][0] == 0:
-                boundary_c[i][j] = 1
-                voltage[i][j] = 0
-            elif (start[0] == i and start[1] == j) or (end[0] == i and end[1] == j):
-                boundary_c[i][j] = 1
-                voltage[i][j] = 255
-            else:
-                boundary_c[i][j] = 0
-                voltage[i][j] = 255
-            if(i == 0 or i == len(image) - 1) or (j == 0 or j == len(image[0] - 1)):
-                boundary_c[i][j] = 1
-                voltage[i][j] = 0
-    for i in range(5000): # todo
-        voltage = averagePoints(boundary_c, voltage)
-        if(i%100 == 0):
-            print(i)
-        #print(voltage)
-    print(voltage)
-    draw_result(image, voltage)
+    kernel = np.array([[0.00, 0.25, 0.00],
+                       [0.25, 0.00, 0.25],
+                       [0.00, 0.25, 0.00]])
 
-    # given voltages, now find the path
+    # Utilizing anthony's idea to use cv2
+    for i in range(15000): # Would have to do a lot more iterations to get this to work with bigger images
+        potential_map = cv2.filter2D(potential_map, -1, kernel)
+        
+        # potential_map[start[1]][start[0]] = 0 - THIS WAS MY MISTAKE
+        potential_map[end[1]][end[0]] = 0
+        for b in range(len(boundary)):
+            potential_map[boundary[b][0]][boundary[b][1]] = 255
+        for y in range(height):
+            potential_map[y][0] = 255
+            potential_map[y][length-1] = 255
+        for x in range(length):
+            potential_map[0][x] = 255
+            potential_map[height-1][x] = 255
+
+    draw_result(image, potential_map)
+
 def draw_result(image, voltage):
-    height = len(image)
-    length = len(image[0])
     result = im.copy()
 
-    result.putpixel((5,5), (255,0,0))
-    result.putpixel((670, 370), (0,255,0))
     gradient_descent(voltage, result)
-    #for x in range(height):
-     #   for y in range(length):
-            # WRITEEEE
-      #      result.putpixel((y,x), (0,0,round(voltage[x][y])))
     
     result.show()
 
@@ -71,14 +62,17 @@ def gradient_descent(voltage, result):
     while(abs(x_coord - end[1]) > 1 or abs(y_coord - end[0]) > 1) and iter > 0:
         if(iter%50 == 0):
             print(str(x_coord) + ","+ str(y_coord)+ "," + str(iter))
-        grad_x = (voltage[round(x_coord+1)][round(y_coord)] - voltage[round(x_coord-1)][round(y_coord)])/255
-        grad_y = (voltage[round(x_coord)][round(y_coord+1)] - voltage[round(x_coord)][round(y_coord-1)])/255
-        magnitude = math.sqrt(grad_x**2 + grad_y**2)
 
-        x_coord = x_coord + 3/magnitude*grad_x
-        y_coord = y_coord + 3/magnitude*grad_y
+        x_grad = (voltage[round(x_coord+1)][round(y_coord)] - voltage[round(x_coord-1)][round(y_coord)])/255
+        y_grad = (voltage[round(x_coord)][round(y_coord+1)] - voltage[round(x_coord)][round(y_coord-1)])/255
+        magnitude = math.sqrt(x_grad**2 + y_grad**2)
 
-        result.putpixel((round(y_coord), round(x_coord)), (255,0,0)) 
+        if(magnitude != 0):
+            x_coord = x_coord-2*x_grad/magnitude
+            y_coord = y_coord-2*y_grad/magnitude
+        
+        result.putpixel((round(y_coord), round(x_coord)), (255,0,0))
+
         iter -= 1
 
 

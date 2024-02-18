e5e499f6fc41edf5d55e549afa2468de023fca1b
Vinay <vprita@cs.washington.edu>
2024-02-17 17:54:36 -0800

    Made simple way to test different parameters' results

--- rrt_expanding_laplace.py
+++ rrt_expanding_laplace.py
@@ -25,13 +25,21 @@ node_list = []
 result_images = []
 
 # Generate a random point in the maze
-def random_point(start, height, length, potential_map):
+def random_point(start, height, length, potential_map, file_name, image, end, prob_of_choosing_start, show_every_attempted_point):
     new_x = random.randint(0, length - 1)#start[1]
     new_y = random.randint(0, height - 1)#start[0]
-    if(random.random() > 0.95):#0.999):
+    if(random.random() > 1 - prob_of_choosing_start):
         new_x = start[1]
         new_y = start[0]
-
+    if(show_every_attempted_point and image[new_y][new_x][0] == 255):
+        im = Image.open(file_name)
+        result = im.copy() # result image
+        draw_result(image, result, start, end, node_list)#draw_result(image, result, start, end, parent_x_array, parent_y_array)
+        for x in range(new_x - 3, new_x + 3):
+            for y in range(new_y - 3, new_y + 3):
+                if(0 < x and x < length - 1 and 0 < y and y < height - 1):
+                    result.putpixel((x, y), (0, 0, 255))
+        result_images.append(result)
     # needToFindNew = False
     # for node in node_list:
     #         if(int(node.y) == int(new_y) and int(node.x) == int(new_x)):
@@ -40,6 +48,15 @@ def random_point(start, height, length, potential_map):
     while potential_map[new_y][new_x] == 1: #or needToFindNew
         new_x = random.randint(0, length - 1)
         new_y = random.randint(0, height - 1)
+        if(show_every_attempted_point and image[new_y][new_x][0] == 255):
+            im = Image.open(file_name)
+            result = im.copy() # result image
+            draw_result(image, result, start, end, node_list)#draw_result(image, result, start, end, parent_x_array, parent_y_array)
+            for x in range(new_x - 3, new_x + 3):
+                for y in range(new_y - 3, new_y + 3):
+                    if(0 < x and x < length - 1 and 0 < y and y < height - 1):
+                        result.putpixel((x, y), (0, 0, 255))
+            result_images.append(result)
         # needToFindNew = False
         # for node in node_list:
         #     if(int(node.y) == int(new_y) and int(node.x) == int(new_x)):
@@ -66,7 +83,7 @@ def LaPlace_average(potential_map, kernel, height, length, boundary_x, boundary_
     return potential_map
 
 # RRT Algorithm
-def RRT(image, start, end, iterations, step_size):
+def RRT(image, start, end, iterations, step_size, file_name, prob_of_choosing_start, la_place_at_start, la_place_each_time, show_every_attempted_point):
     height = len(image)
     length = len(image[0])
 
@@ -79,8 +96,7 @@ def RRT(image, start, end, iterations, step_size):
     pathFound = False
 
     potential_map = np.ones((height, length))
-    potential_map[end[1]][end[0]] = 0 # Goal
-    # Get the black walls
+    potential_map[end[1]][end[0]] = 0 
     boundary_x = []
     boundary_y = []
     for y in range(height):
@@ -93,37 +109,35 @@ def RRT(image, start, end, iterations, step_size):
     kernel = np.array([[0.00, 0.25, 0.00],
                        [0.25, 0.00, 0.25],
                        [0.00, 0.25, 0.00]])
-    for _ in range(250):
+    for _ in range(la_place_at_start):
         potential_map = LaPlace_average(potential_map, kernel, height, length, boundary_x, boundary_y, end, node_list)
     while pathFound == False:
         total_iter = total_iter + 1
 
         if(total_iter == iterations):
             print("Iteration limit exceeded.")
-            return node_list#node_list[i - 1].parent_x, node_list[i - 1].parent_y
-            #break
+            return node_list
 
         # Get random point
-        new_x, new_y, potential_map = random_point(start, height, length, potential_map)
+        new_x, new_y, potential_map = random_point(start, height, length, potential_map, file_name, image, end, prob_of_choosing_start, show_every_attempted_point)
         
-        for _ in range(250):
+        for _ in range(la_place_each_time):
             potential_map = LaPlace_average(potential_map, kernel, height, length, boundary_x, boundary_y, end, node_list)
 
         new_node_list = try_grad_descent(potential_map, step_size, new_x, new_y, node_list)
         i = i + len(new_node_list)
         node_list.extend(new_node_list)
         # HERE - Drawing image step by step
-        if(len(new_node_list) != 0):
-
-            im = Image.open("world4.png")
-            result = im.copy() # result image
-            draw_result(image, result, start, end, node_list)#draw_result(image, result, start, end, parent_x_array, parent_y_array)
-            for x in range(new_x - 3, new_x + 3):
-                for y in range(new_y - 3, new_y + 3):
-                    if(0 < x and x < length - 1 and 0 < y and y < height - 1):
-                        result.putpixel((x, y), (0, 255, 255))
-            result_images.append(result)
-        # TO HERE
+        im = Image.open(file_name)
+        result = im.copy() # result image
+        draw_result(image, result, start, end, node_list)
+        for x in range(new_x - 3, new_x + 3):
+            for y in range(new_y - 3, new_y + 3):
+                if(0 < x and x < length - 1 and 0 < y and y < height - 1):
+                    result.putpixel((x, y), (0, 255, 255))
+        result_images.append(result)
+        #TO HERE
+        
         if len(new_node_list) != 0 and int(new_x) == start[0] and int(new_y) == start[1]:
             pathFound = True
     return node_list
@@ -131,20 +145,15 @@ def RRT(image, start, end, iterations, step_size):
 def try_grad_descent(potential_map, step_size, new_x, new_y, node_list):
     poser = new_y
     posec = new_x
-    toAppend = Node(int(posec), int(poser))
-        # toAppend.parent_x.append(node_list[limit].x)
-        # toAppend.parent_y.append(node_list[limit].u)
-    
+    toAppend = Node(int(posec), int(poser))    
 
     limit = 0
     new_node_list = []
     new_node_list.append(toAppend)
     while True:
         if limit > 1000:
-            # print('Path not found! Maybe try providing more Laplace iterations!')
             print("Couldn't find - Stuck :(")
-            break
-
+            return []
         gradr = potential_map[round(poser+1)][round(posec)] - potential_map[round(poser-1)][round(posec)]
         gradc = potential_map[round(poser)][round(posec+1)] - potential_map[round(poser)][round(posec-1)]
         maggrad = math.sqrt(gradr**2 + gradc**2)
@@ -156,20 +165,10 @@ def try_grad_descent(potential_map, step_size, new_x, new_y, node_list):
         print(poser)
         print(posec)
         toAppend = Node(int(posec), int(poser))
-        # toAppend.parent_x.append(node_list[limit].x)
-        # toAppend.parent_y.append(node_list[limit].u)
         new_node_list.append(toAppend)
         limit = limit + 1
 
-        # if(potential_map[int(poser)][int(posec)] == 0):
-        #     print("Found from ")
-        #     print(new_x)
-        #     print(new_y)
-        #     print("to")
-        #     print(poser)
-        #     print(posec)
-        #     return new_node_list
-            
+
         for node in node_list:
             if(node.y-step_size-1 < poser and poser < node.y+step_size+1 and node.x-step_size-1 < posec and posec < node.x+step_size+1):
                 
@@ -180,21 +179,16 @@ def try_grad_descent(potential_map, step_size, new_x, new_y, node_list):
                 print(node.x)
                 print(node.y)
                 return new_node_list
-               # break
-
-        # if(new_y-step_size-1 < poser and poser < new_y+step_size+1 and new_x-step_size-1 < posec and posec < new_x+step_size+1):
-        #     print("Stuck :(")
-        #     return []
 
 
-# def draw_result(image, result, start, end, parent_x_array, parent_y_array):
+
 def draw_result(image, result, start, end, node_list):
     height = len(image)
     length = len(image[0])
 
     # Draw all of the nodes in the RRT
     for node in node_list:
-        print(node)
+        #print(node)
         round_x = math.floor(node.x)
         round_y = math.floor(node.y)
         for x in range(round_x - 1, round_x + 1):
@@ -215,8 +209,8 @@ def draw_result(image, result, start, end, node_list):
                 result.putpixel((x, y), (0, 0, 255))
                 
 def main():
-    conda = input("This is just for VSCode using the Conda Python version, type anything here: ")
     image = input("Enter the name of your image file (include .jpg, .png, etc.): ")
+    file_name = image
 
     # Get image and convert to 2D array
     im = Image.open(image)
@@ -238,36 +232,29 @@ def main():
 
     iterations = int(input('Enter the number of iterations (10000 recommended): '))
     step_size = int(input('Enter the step size of the RRT (3 recommended): '))
+    la_place_at_start = int(input('Enter the LaPlace iters before randomly selecting points: '))
+    la_place_each_time = int(input('Enter the LaPlace iters after each point explored: '))
+    prob_of_choosing_start = float(input('Enter the probability of choosing the start: '))
+    show_every_attempted_point = ("y" == input('Enter "y" to show every attempted point in video: '))
+    output_path = input('Enter the desired video file name (include .mp4 at the end, make it descriptive based on the parameters entered): ')
+    fps = int(input('Enter the fps of video (recommended 10 for small videos, 30+ for longer): '))
 
-    node_list = RRT(image, start, end, iterations, step_size)#parent_x_array, parent_y_array = RRT(image, start, end, iterations, step_size)
+    node_list = RRT(image, start, end, iterations, step_size, file_name, prob_of_choosing_start, la_place_at_start, la_place_each_time, show_every_attempted_point)
 
-    result = im.copy() # result image
-    draw_result(image, result, start, end, node_list)#draw_result(image, result, start, end, parent_x_array, parent_y_array)
+    print("Drawing the result...")
+    result = im.copy() 
+    draw_result(image, result, start, end, node_list)
+    im = Image.open(file_name)
+    result_images.append(result)
     result.show()
-
-    output_path = 'output_video.mp4'
-    #images.sort()  # Sort the images by name (ensure they are in the correct order)
-
-    # Create a writer object
-    writer = imageio.get_writer(output_path, fps=30)
+    
+    writer = imageio.get_writer(output_path, fps=fps)
 
     for image_filename in result_images:
-        # image_path = os.path.join("", image_filename)
-        # image = imageio.imread(image_path)
         writer.append_data(np.array(image_filename))
     
     writer.close()
 
-    # #videodims = (700,500)
-    # fourcc = cv2.VideoWriter_fourcc(*'avc1')    
-    # video = cv2.VideoWriter("test.mp4",fourcc, 60,(len(image), len(image[0])))
-    # #img = Image.new('RGB', videodims, color = 'darkred')
-    # #draw stuff that goes on every frame here
-    # for result_image in result_images:
-    #     #imtemp = img.copy()
-    #     # draw frame specific stuff here.
-    #     video.write(cv2.cvtColor(np.array(result_image), cv2.COLOR_RGB2BGR))
-    # video.release()
 
 if __name__ == '__main__':
     main()
